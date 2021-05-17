ccd4015cc6d64130f32129764653dedc7b9756d2
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-17 14:10:33 -0300

    Study package java-io

---
+++ src/TesteLeitura.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class TesteLeitura {
+    public static void main(String[] args) throws IOException {
+
+        //Fluxo de Entrada com Arquivo
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br =  new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        System.out.println(linha);
+    }
+}
