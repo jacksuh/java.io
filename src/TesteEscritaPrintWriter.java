c6df287c703ba218fac948e2bb7ae3662d134760
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-18 12:38:35 -0300

    Study package java-io Scanner

---
+++ src/TesteEscritaPrintWriter.java
@@ -0,0 +1,32 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class TesteEscritaPrintWriter {
+
+    public static void main(String[] args) throws IOException {
+
+        //Fluxo de Entrada com Arquivo
+        //FileOutputStream fos = new FileOutputStream("lorem2.txt");
+        //Writer osw = new OutputStreamWriter(fos);
+        //BufferedWriter bw =  new BufferedWriter(osw);
+        //FileWriter fw = new FileWriter("lorem4.txt");
+        //caso foi utilizar o Buffered podemos voltar a utilizar o bw.newline.
+        //BufferedWriter bw = new BufferedWriter(fw);
+
+        //PrintStream ps = new PrintStream(new File("lorem2.txt"));
+
+        //linheSeparator "Fazer nova linha"
+
+        PrintWriter ps = new PrintWriter("lorem5.txt");
+
+        ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        ps.println();
+
+        ps.println("asfasdfsafdas dfs sdf asf assdß");
+
+        ps.close();
+
+    }
+}
---
+++ src/TesteLeituraScanner.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TesteLeituraScanner {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("contas.csv"));
+        while(scanner.hasNextLine()) {
+            String linha = scanner.nextLine();
+            System.out.println(linha);
+        }
+
+        scanner.close();
+
+    }
+}
