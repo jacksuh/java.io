26c6a808097ccf1f3797dce735e1682a0ef964ce
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-19 11:12:42 -0300

    java-io Format

--- src/TesteLeituraScanner.java
+++ src/TesteLeituraScanner.java
@@ -15,14 +15,14 @@ public class TesteLeituraScanner {
             linhaScanner.useLocale(Locale.US);
             linhaScanner.useDelimiter(",");
 
-            String valor1 = linhaScanner.next();
-            int valor2 = linhaScanner.nextInt();
-            int valor3 = linhaScanner.nextInt();
-            String valor4 = linhaScanner.next();
-            double valor5 = linhaScanner.nextDouble();
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
 
-            System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
-
+            System.out.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %05.2f %n",
+                    tipoConta, agencia, numero, titular, saldo);
             linhaScanner.close();
 
 //                        String[] valores = linha.split(",");
